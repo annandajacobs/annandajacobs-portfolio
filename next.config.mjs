9421ab5eeb2d8579a7dfcb49fd496de369e5ce29
annandajacobs <annandajacobs@gmail.com>
2026-09-11 18:04:21 -0300

    feat: configurar deploy no github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: "export",
+
+  basePath: "/annandajacobs-portfolio",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
